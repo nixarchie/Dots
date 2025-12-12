5076aba52b2e4ab7995f9ebf7f05ade79e663540
nixarchie <85381132+nixarchie@users.noreply.github.com>
2025-12-12 21:47:46 +0530

    Changed `setup_shell.sh`

--- scripts/bash/setup_shell.sh
+++ scripts/bash/setup_shell.sh
@@ -1,6 +1,17 @@
 #!/usr/bin/env bash
 set -e
 
+log()   { echo -e "\033[1;32m[INFO]\033[0m $(pretty_path "$1")"; }
+pretty_path() {
+    local path="$1"
+    # Absolute path starting with $HOME → replace with ~
+    if [[ "$path" == "$HOME"* ]]; then
+        echo "~${path#$HOME}"
+    else
+        echo "$path"
+    fi
+}
+
 log "Sourcing shell configs..."
 
 case "$USER_SHELL" in
