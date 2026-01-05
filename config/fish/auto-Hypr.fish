2fea3962b34427c7be1075f4d27d456fc88b333f
Nixie <85381132+nixarchie@users.noreply.github.com>
2026-01-05 21:30:44 +0530

    changed fish_variables

--- config/fish/auto-Hypr.fish
+++ config/fish/auto-Hypr.fish
@@ -1,7 +1,5 @@
-# !/usr/bin/fish
-
 # Auto start Hyprland on tty1
-if test -z "$DISPLAY"; and test "$XDG_VTNR" -eq 1
+if test -z "$DISPLAY" ;and test "$XDG_VTNR" -eq 1
     mkdir -p ~/.cache
-    exec Hyprland >~/.cache/hyprland.log 2>&1
+    exec start-hyprland > ~/.cache/hyprland.log 2>&1
 end
